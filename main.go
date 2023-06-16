a3502f452feee5483c0253f1af7e123728a53b9b
kang-on-working <kang.onworking@gmail.com>
2023-06-16 23:07:20 +0900

    Refactoring Exception Handling Syntax

--- main.go
+++ main.go
@@ -12,9 +12,7 @@ func main() {
 
 	fmt.Println("main")
 	FILEDATA, err := pkg.ReadFile(target)
-	if err != nil {
-		return
-	} //에러는 함수 단에서 이미 출력될 것
+	if err != nil { return } //에러는 함수 단에서 이미 출력될 것
 
 	fmt.Println(string(FILEDATA))
 
--- pkg/iocontrol.go
+++ pkg/iocontrol.go
@@ -10,16 +10,9 @@ func CheckFileAccessibility(file_path string) error {
 	//file accessiblity check
 	file, err := os.Open(file_path)
 	if err != nil {
-		if os.IsPermission(err) {
-			fmt.Println("File Permission Denied : ", err)
-			return err
-		} else if os.IsNotExist(err) {
-			fmt.Println("File Not Exist : ", err)
-			return err
-		} else {
-			fmt.Println("Undefined Error : ", err)
-			return err
-		}
+		if os.IsPermission(err) { fmt.Println("File Permission Denied : ", err) ; return err
+		} else if os.IsNotExist(err) { fmt.Println("File Not Exist : ", err) ; return err
+		} else { fmt.Println("Undefined Error : ", err) ; return err }
 	} //errors
 	defer file.Close()
 	return nil
@@ -27,25 +20,17 @@ func CheckFileAccessibility(file_path string) error {
 
 func ReadFile(file_path string) ([]byte, error) {
 
-	if err := CheckFileAccessibility(file_path); err != nil {
-		return nil, err
-	}
+	if err := CheckFileAccessibility(file_path); err != nil { return nil, err }
 
 	content, err := ioutil.ReadFile(file_path)
-	if err != nil {
-		fmt.Println("File Reading Error : ", err)
-		return nil, err
-	}
+	if err != nil { fmt.Println("File Reading Error : ", err) ; return nil, err	}
 
 	return content, nil
 }
 
 func WriteFile(file_path string, data []byte) error {
 
-	if err := ioutil.WriteFile(file_path, data, 0644); err != nil {
-		fmt.Println("File Write Error : ", err)
-		return err
-	}
+	if err := ioutil.WriteFile(file_path, data, 0644); err != nil { fmt.Println("File Write Error : ", err) ; return err }
 
 	return nil
 }
